3ef7a790aa49b19f2a288fa6f0d7cbcff89b2936
Andres Arevalo <andresfelipear@gmail.com>
2022-01-05 18:35:59 -0800

    created form write-message and manage method post

--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,28 @@
 const path = require('path')
 const router = require('express').Router()
+const fs = require('fs')
 
 const rootDirectory = require('../util/path')
 
-router.get('/', (req,res,next)=>{
+router.get('/', (req, res, next) => {
     res.sendFile(path.join(rootDirectory, 'views', 'index.html'))
 })
+
+router.get('/write-message', (req, res, next) => {
+    res.sendFile(path.join(rootDirectory, 'views', 'write-message.html'))
+})
+
+router.post('/message', (req, res, next) => {
+    const message = req.body.message
+    fs.writeFile('lect-01.txt', message, (err) => {
+    if (err) throw err
+    res.statusCode = 302
+    res.redirect('/')
+})
+    console.log(req.body)
+
+})
+
+
 
 module.exports = router
